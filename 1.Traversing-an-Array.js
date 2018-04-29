6dbad6bb07a5a57d12603df7fd7ebe7f0812ca26
vikashraj144 <vikashraj144@gmail.com>
2018-04-29 10:10:37 +0530

    traversing added

---
+++ 1.Traversing-an-Array.js
@@ -0,0 +1,9 @@
+/*
+The Array is Javascript's only collection type
+Array already has the map, filter, and reduce functions
+*/
+var names = ["Ben", "Ben2", "Ben3", "Priya", "rian"];
+
+for(let counter = 0; counter < names.length; counter++) {
+    console.log(names[counter]);
+}
